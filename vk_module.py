a668f0a901c50df781706bd70f979e08563e83a5
pointhex <1111artem1111@gmail.com>
2017-03-02 00:10:31 +0300

    I figured out of curses

--- vk_module.py
+++ vk_module.py
@@ -1,7 +1,10 @@
 import vk
 import json
 import time
-#import vk_my_auth 
+import array
+import select
+import sys
+import curses
 from vk_my_auth import get_access_token
 from vk_my_auth import auth
 
@@ -9,22 +12,53 @@ client_id = 5821652
 
 session = vk.AuthSession(access_token = get_access_token())
 vkapi = vk.API(session)
-#vkapi.wall.post(message="Hello, world")p
 
 def searchForUser(user_list, ID):
     for user in user_list:
         user_obj = vkapi.users.get(user_ids = user)
         print(user_obj[0]['first_name'])
-        time.sleep(0.3)
+        time.sleep(0.4)
         if type(user) is int:
             continue
 
-user_list = vkapi.friends.get(user_ids=client_id)
-while True:
-    time.sleep(1);
-    message_list = vkapi.messages.get(time_offset = 0)
+title_array = [];
+
+def searchTitle(title_array, new_title):
+    for title in title_array:
+        if title == new_title:
+            return False
+    title_array.append(new_title)
+    return True
+
+myscreen = curses.initscr()
+title = "ss"
+while True: 
+    myscreen.border(0) 
+    myscreen.refresh() 
+    curses.noecho()
+    myscreen.addstr(title.decode("ascii") + "\n")
+    print myscreen.getch()
+    #myscreen.clear()
+    curses.endwin()
+    #print sys.stdin.read(1)
+    #sys.stdout.flush()
+    time.sleep(0.3);
+    message_list = vkapi.messages.get(time_offset = 0, count = 200)
     for message in message_list:
         if not type(message) is int:
             if message['read_state'] != 1:
-                print(message['body'])
+                #print(message)
+                if  message['title'] == ' ... ':
+                    time.sleep(0.3);
+                    message_user = vkapi.users.get(user_ids = message['uid'])
+                    title = message_user[0]['first_name'] + ' ' + message_user[0]['last_name'] 
+                else:
+                    title = message['title']
+
+                if searchTitle(title_array, title):
+                    print title
+#                print message['title'].rjust(25), message_user[0]['first_name'].rjust(10), message_user[0]['last_name'].rjust(10)
+#                print(message['body'])
+
+#user_list = vkapi.friends.get(user_ids=client_id)
 #searchForUser(user_list, "dsds")
