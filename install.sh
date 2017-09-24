0a2e5d8f9b2dcbc0bed9e2f70d95016ad86f651d
poinhex <1111artem1111@gmail.com>
2017-09-24 21:09:13 +0300

    Vertion 1.3
    Rename 'update' to 'init'.
    Add new function 'change' to rename your contacts.
    Change app home directory.

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ rm /usr/share/bash-completion/completions/vk
 rm /usr/bin/vk_my_auth.py
 rm /usr/bin/vk
 
+mkdir /usr/vk
+
 cp ./complete.sh /usr/share/bash-completion/completions/vk
 cp ./vk_module.py /usr/bin/vk
 cp ./vk_my_auth.py /usr/bin/vk_my_auth.py
+cp ./users /usr/vk/users
+cp ./token /usr/vk/token
--- vk_module.py
+++ vk_module.py
@@ -6,30 +6,32 @@ import array
 import sys
 import vk_my_auth
 
+home_dir = '/usr/vk'
+
 def get_access_token():
     regs = vk.AuthSession(vk_my_auth.m_client_id, vk_my_auth.m_email, vk_my_auth.m_password, scope = 'messages')
     return regs.get_access_token()
 
 def login():
     try:
-            f = open('token', 'r')
+            f = open(home_dir+'token', 'r')
     except IOError as e:
-            f = open('token', 'w')
+            f = open(home_dir+'token', 'w')
             f.close()
-            f = open('token', 'r')
+            f = open(home_dir+'token', 'r')
     if f.read == '':
         session = vk.AuthSession(access_token = f.read())
         f.close();
     else:
         f.close()
-        f = open('token', 'w')
+        f = open(home_dir+'token', 'w')
         token = get_access_token();
         f.write(token)
         session = vk.AuthSession(access_token = token)
     return vk.API(session, v='5.68')
 
 def update_user_list(user_list):
-    f = open('users', 'w')
+    f = open(home_dir+'users', 'w')
     for user in user_list["items"]:
         user_obj = vkapi.users.get(user_ids = user)
         print(user_obj[0]['first_name'] + '_' + user_obj[0]['last_name'] + ' ')
@@ -41,7 +43,7 @@ def update_user_list(user_list):
     f.close()
 
 def find_user_name(message):
-    f = open('users', 'r')
+    f = open(home_dir+'users', 'r')
     new_id = ''
     if message['title'] == '':
         user_obj = vkapi.users.get(user_ids = message['user_id'])
@@ -55,7 +57,7 @@ def find_user_name(message):
                 return
     f.close()
     if message['title'] != '':
-        f = open('users', 'a')
+        f = open(home_dir+'users', 'a')
         f.write( 'Group_' + message['title'].replace(' ', '').replace('_', '') \
                 + '_' + str(2000000000 + message['chat_id']) + ' ')
         f.write( message['title'].replace(' ', '').replace('_', '') + '_Group' \
@@ -64,7 +66,7 @@ def find_user_name(message):
         f.close()
         return
 
-    f = open('users', 'a')
+    f = open(home_dir+'users', 'a')
     f.write(user_obj[0]['first_name'] + '_' + user_obj[0]['last_name'] + '_' + \
                 str(user_obj[0]['id']) + ' ')
     f.write(user_obj[0]['last_name'] + '_' + user_obj[0]['first_name'] + '_' + \
@@ -92,15 +94,27 @@ if (len(sys.argv) > 1):
     if (sys.argv[1] == 'get'):
         get_messages()
 
-    if (sys.argv[1] == 'update'):
+    if (sys.argv[1] == 'init'):
         user_list = vkapi.friends.get(user_ids=vk_my_auth.m_client_id)
         update_user_list(user_list)
 
     if (sys.argv[1] == 'send'):
         vkapi.messages.send(peer_id=sys.argv[2].split('_')[2], message = sys.argv[3])
+        vkapi.messages.markAsRead(peer_id=sys.argv[2].split('_')[2], answered=1)
 
     if (sys.argv[1] == 'clear'):
         vkapi.messages.markAsRead(peer_id=sys.argv[2].split('_')[2], answered=1)
+
+    if (sys.argv[1] == 'change'):
+        f = open(home_dir+'users', 'a')
+        if (len (sys.argv[3].split('_')) == 1):
+            f.write(sys.argv[3] + '__' + sys.argv[2].split('_')[2])
+        elif (len(sys.argv[3].split('_')) == 2):
+            f.write(sys.argv[3] + '_' + sys.argv[2].split('_')[2])
+        else:
+            print('Enter less _')
+
+        f.close()
 else :
     print('Enter a command!')
     print('update : Update friend list for hints')
